2fc0bbd3df1886e058c31a2d259aac9f92a1edab
dev637 <devang713@gmail.com>
2020-12-12 11:26:38 -0500

    Update gatsby with wrapPageElement

--- gatsby/src/pages/404.js
+++ gatsby/src/pages/404.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export default function FourOhFourPage() {
-  return (
-    <div>
-      <p>Hey - That page doesn't exist!!!</p>
-    </div>
-  );
+  return <p>Hey - That page doesn't exist!!!</p>;
 }

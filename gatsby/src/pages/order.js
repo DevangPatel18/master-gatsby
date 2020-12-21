6dd3717135431dade8f49b0265c14c479e1a6ec5
dev637 <devang713@gmail.com>
2020-12-21 12:41:37 -0500

    Create submit order handler

--- gatsby/src/pages/order.js
+++ gatsby/src/pages/order.js
@@ -14,14 +14,26 @@ import calculateOrderTotal from '../utils/calculuateOrderTotal';
 export default function OrderPage({ data }) {
   const { values, updateValue } = useForm({ name: '', email: '' });
   const pizzas = data.pizzas.nodes;
-  const { order, addToOrder, removeFromOrder } = usePizza({
+  const {
+    order,
+    addToOrder,
+    removeFromOrder,
+    error,
+    loading,
+    message,
+    submitOrder,
+  } = usePizza({
     pizzas,
-    inputs: values,
+    values,
   });
+
+  if (message) {
+    return <p>{message}</p>;
+  }
   return (
     <>
       <SEO title="Order a Pizza!" />
-      <OrderStyles>
+      <OrderStyles onSubmit={submitOrder}>
         <fieldset>
           <legend>Your Info</legend>
           <label htmlFor="name">
@@ -87,7 +99,10 @@ export default function OrderPage({ data }) {
           <h3>
             Your Total is {formatMoney(calculateOrderTotal(order, pizzas))}
           </h3>
-          <button type="submit">Order Ahead</button>
+          <div>{error ? <p>Error: {error}</p> : ''}</div>
+          <button type="submit" disabled={loading}>
+            {loading ? 'Placing Order...' : 'Order Ahead'}
+          </button>
         </fieldset>
       </OrderStyles>
     </>
--- gatsby/src/utils/usePizza.js
+++ gatsby/src/utils/usePizza.js
@@ -1,12 +1,18 @@
 import { useContext, useState } from 'react';
 
 import OrderContext from '../components/OrderContext';
+import attachNamesAndPrices from './attachNamesAndPrices';
+import calculateOrderTotal from './calculuateOrderTotal';
+import formatMoney from './formatMoney';
 
-export default function usePizza({ pizzas, inputs }) {
+export default function usePizza({ pizzas, values }) {
   // 1. Create some state to hold our order
   // We got rid of this line because we moved usestate up to the provider
   // Now we access both our state and our update function (setOrder) via context
   const [order, setOrder] = useContext(OrderContext);
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
   // 2. Make a function add things to order
   function addToOrder(orderedPizza) {
     setOrder([...order, orderedPizza]);
@@ -19,13 +25,53 @@ export default function usePizza({ pizzas, inputs }) {
       // everything after the item we want to remove
       ...order.slice(index + 1),
     ]);
-    // 4. Send this data the a serverless functino when they check out
   }
-  // TODO
+
+  // this is the function that is run when someone submits the form
+  async function submitOrder(e) {
+    e.preventDefault();
+    console.log(e);
+    setLoading(true);
+    setError(null);
+    setMessage(null);
+    // gather all the data
+    const body = {
+      order: attachNamesAndPrices(order, pizzas),
+      total: formatMoney(calculateOrderTotal(order, pizzas)),
+      name: values.name,
+      email: values.email,
+    };
+    // 4. Send this data the a serverless functino when they check out
+    const res = await fetch(
+      `${process.env.GATSBY_SERVERLESS_BASE}/placeOrder`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      }
+    );
+    const text = JSON.parse(await res.text());
+
+    // check if everything worked
+    if (res.status >= 400 && res.status < 600) {
+      setLoading(false); // turn off loading
+      setError(text.message);
+    } else {
+      // it worked!
+      setLoading(false);
+      setMessage('Success! Come on down for your pizza');
+    }
+  }
 
   return {
     order,
     addToOrder,
     removeFromOrder,
+    error,
+    loading,
+    message,
+    submitOrder,
   };
 }
